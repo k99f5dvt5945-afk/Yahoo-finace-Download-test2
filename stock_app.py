32f8ac967decc9ec308f6c8eeb6aa0d5fc0248d4
JayChen <k99f5dvt5945@gmail.com>
2026-09-22 07:29:14 +0000

    Add Yahoo Finance data fetching

--- stock_app.py
+++ stock_app.py
@@ -1,2 +1,18 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
 stock_code = input("請輸入股票代號：")
+
 print("你輸入的股票代號是：", stock_code)
+
+data = yf.download(stock_code, period="1mo")
+
+print(data[["Close", "Volume"]])
+
+plt.plot(data.index, data["Close"])
+plt.title(f"{stock_code} Closing Price")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
