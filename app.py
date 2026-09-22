1980061e288f0e8d32c8506911b050b9613f7c69
JayChen <k99f5dvt5945@gmail.com>
2026-09-22 07:48:35 +0000

    Add historical period selector

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ st.title("台股歷史資料查詢")
 
 stock_code = st.text_input("請輸入股票代號", "2330.TW")
 
+period_option = st.selectbox(
+    "查詢期間",
+    ["從最早資料到最新資料", "1 個月", "3 個月", "6 個月", "1 年", "5 年"]
+)
+
 if st.button("查詢"):
-    data = yf.download(stock_code, period="1mo")
+
+    if period_option == "從最早資料到最新資料":
+        data = yf.download(stock_code, period="max")
+    elif period_option == "1 個月":
+        data = yf.download(stock_code, period="1mo")
+    elif period_option == "3 個月":
+        data = yf.download(stock_code, period="3mo")
+    elif period_option == "6 個月":
+        data = yf.download(stock_code, period="6mo")
+    elif period_option == "1 年":
+        data = yf.download(stock_code, period="1y")
+    elif period_option == "5 年":
+        data = yf.download(stock_code, period="5y")
 
     st.write("股票代號：", stock_code)
 
